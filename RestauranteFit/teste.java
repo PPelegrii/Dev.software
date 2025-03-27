8c4df2e6fec3a0e49364fc49d60da8210598ebf8
PPelegrii <pedropelegri2005@gmail.com>
2025-03-27 10:52:04 -0300

    .

---
+++ RestauranteFit/teste.java
@@ -0,0 +1,36 @@
+package RestauranteFit;
+ /* 
+  * blah blah blah teste teste não compila blah
+  */
+ 
+ public class teste {
+     public static void main(String[] args) {
+         // Cria um usuário
+         Usuario usuario = new Usuario();
+         //usuario.Cadastrar();
+         //usuario.gerarID();
+         //String id = usuario.pegarID();
+         //System.out.println("ID do usuário: " + id);
+         usuario.Cadastrar();
+         usuario.gerarID();
+         String id = usuario.pegarID();
+         System.out.println("ID do usuário: " + id);
+ 
+         //Produto produto = new Produto();
+         //produto.cadastrarProduto();
+         //produto.listarProdutos();
+         //produto.verificarEstoque(); // :]
+         Produto produto = new Produto(); // ta com erro
+         produto.cadastrarProduto();
+         produto.listarProdutos();
+         produto.verificarEstoque(); // :]
+ 
+         Pedido pedido = new Pedido(usuario);
+         pedido.fazerPedido();
+ 
+         Pagamento pagamento = new Pagamento(pedido);
+         pagamento.realizarPagamento();
+ 
+         pagamento.exibirDetalhesPagamento();
+     }
+ }
